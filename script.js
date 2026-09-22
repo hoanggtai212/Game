162ec88731d10c44bb5dd699083337ace547e65e
HT <rongchube99@gmail.com>
2026-09-22 22:22:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const modalTitle=document.getElementById("modalTitle");
 let selected=null;
 
 const chapters=[
-{name:"Chặng 02",range:"Bài 5–7",from:5,positions:[[58,13],[48,28],[48,67]]},
+{name:"Chặng 02",range:"Bài 5–7",from:5,positions:[[58,16],[48,40],[46,63]]},
 {name:"Chặng 03",range:"Bài 8–10",from:8,positions:[[58,36],[48,62],[48,88]]},
 {name:"Chặng 04",range:"Bài 11–13",from:11,positions:[[58,36],[48,62],[48,88]]},
 {name:"Chặng 05",range:"Bài 14–16",from:14,positions:[[58,36],[48,62],[48,88]]},
